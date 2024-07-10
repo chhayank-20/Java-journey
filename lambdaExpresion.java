0083d67f5d4c3160f88ee2d99ef93992436e0767
youngman20 <chhayankpatel209@gmail.com>
2024-07-10 16:04:39 +0530

    Added file

---
+++ lambdaExpresion.java
@@ -0,0 +1,33 @@
+package Demo;
+
+@FunctionalInterface interface I{
+    void show (int a );
+}
+
+@FunctionalInterface interface B{
+    void test (int a );
+}
+
+class A  {
+    static void show(int a){
+        System.out.println("class A , method show with value : "+a);
+    }
+    void test(int b){
+        System.out.println("method : Test with value :"+b);
+    }
+}
+
+public class lambdaExpresion {
+    public static void main(String args[]){
+        I obj2 = a -> System.out.println(a);
+        obj2.show(4);
+
+    // with static method 
+        I obj = A::show;
+        obj.show(7);
+    // without static method (with object)
+        A obj3 = new A();
+        B obj4 = obj3::test;
+        obj4.test(5);
+    }
+}
